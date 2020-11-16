dfdfbcb6b33b1ddb7d751dc4d40d7017e85fc513
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-11-16 19:04:59 +0100

    Timer change

--- Assets/02_Scripts/Entities/Prototype/Enemy/Utilities/AIUtilities.cs
+++ Assets/02_Scripts/Entities/Prototype/Enemy/Utilities/AIUtilities.cs
@@ -10,6 +10,7 @@ public class AIUtilities : MonoBehaviour
     {
         float currentTime;
         float waitTime;
+        bool timerDone;
 
         public Timer()
         {
@@ -19,13 +20,13 @@ public class AIUtilities : MonoBehaviour
         public Timer(float time)
         {
             waitTime = time;
-            currentTime = waitTime;
+            currentTime = 0;
         }
 
-        
+
         public void StartTimer()
         {
-            currentTime = waitTime;
+            timerDone = false;
 
             //Starting the async function
             Timing();
@@ -35,23 +36,28 @@ public class AIUtilities : MonoBehaviour
         //Subtracts deltatime every frame/playerloop
         async UniTask Timing()
         {
-            while (currentTime >= 0)
+            while (currentTime <= waitTime)
             {
-                currentTime -= Time.deltaTime;
+                currentTime += Time.deltaTime;
                 await UniTask.Yield();
+                timerDone = true;
             }
+            currentTime -= waitTime;
         }
 
         //Can be called to check if the timer has counted down to 0
         public bool TimerDone()
         {
-            return currentTime <= 0;
+            return timerDone;
         }
 
         public void setWaitTime(float time)
         {
             waitTime = time;
-            currentTime = waitTime;
+            if (currentTime >= waitTime)
+                currentTime -= waitTime;
+            else
+                currentTime = 0;
         }
     }
 }
