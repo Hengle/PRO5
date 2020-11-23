ee40c42af4416aeb673a856a4454e728929776c3
Rathalin <danielflo1997@hotmail.com>
2020-11-23 15:11:21 +0100

    Add some powerup scripts

--- Assets/02_Scripts/Entities/Prototype/PowerUps/PowerUpController.cs
+++ Assets/02_Scripts/Entities/Prototype/PowerUps/PowerUpController.cs
@@ -7,6 +7,7 @@ public class PowerUpController : MonoBehaviour
 
     private PowerUp _currentPowerUp = null;
 
+
     public void StorePowerUp(PowerUp powerup)
     {
         Debug.Log(string.Format("Stored {0}", powerup));
