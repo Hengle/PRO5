f18a70d775323d6d87d09b59af3748b20e75b473
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-11-23 16:40:26 +0100

    Transfering old UI, fixes $136
    Added Dash cooldown and Healthbar UI

---
+++ Assets/02_Scripts/UI/ProgressBarHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarHealth : ProgessBar
+{
+    public PlayerStatistics player;
+
+    public FloatVariable currentPlayerHealth;
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerStatistics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+
+    public override void GetCurrentFill()
+    {
+        maximum = (player.GetStatValue(StatName.MaxHealth));
+        current = currentPlayerHealth.Value;
+        float fillAmount = (float) current / (float) maximum;
+        slider.value = fillAmount;
+    }
+}
