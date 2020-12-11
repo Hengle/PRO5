9c93025520b2a1bcfb93d484d83b01cc0594c4b2
Rathalin <danielflo1997@hotmail.com>
2020-12-12 00:43:37 +0100

    Change a commend

--- Assets/02_Scripts/Entities/Prototype/PowerUps/EnemyPowerup.cs
+++ Assets/02_Scripts/Entities/Prototype/PowerUps/EnemyPowerup.cs
@@ -10,7 +10,7 @@ public class EnemyPowerup : PowerUp
     {
         List<GameObject> enemies = new List<GameObject>();
         RaycastHit[] hits = Physics.SphereCastAll(_player.transform.position, radius, _player.transform.forward, radius + 1f);
-        // Get all gameobject with EnemyBody attached
+        // Get all gameobject with EnemyBody script attached
         return hits
             .Where(h => h.transform.GetComponent<EnemyBody>())
             .Select(h => h.transform.gameObject)
