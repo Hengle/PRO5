7bea00a70a9473ef033ae1103e6bc3115a71b710
Rathalin <danielflo1997@hotmail.com>
2021-01-19 16:29:00 +0100

    Add SceneManager to persistent scene

--- Assets/02_Scripts/Entities/Prototype/SceneManagement/ScenenManager.cs
+++ Assets/02_Scripts/Entities/Prototype/SceneManagement/ScenenManager.cs
@@ -5,8 +5,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScenenManager : MonoBehaviour
+
 {
+    #region Editor Attributes 
+
+
+    public SceneType startScene = SceneType.Level;
+
+    /// <summary>
+    /// Holds the build index of the start menu scene;
+    /// </summary>
+    public int startMenuScene;
+
+    /// <summary>
+    /// Holds the build index of the level scene
+    /// </summary>
+    public int baseScene;
+
+    /// <summary>
+    /// Holds all build indices of level scenens
+    /// </summary>
+    public List<int> levelScenes = new List<int>();
+
+    #endregion Editoe Attributes
+
     #region Properties and Fields
+
+    public enum SceneType
+    {
+        StartMenu = 0,
+        Level = 1,
+    }
 
 
     /// <summary>
@@ -14,52 +43,116 @@ public class ScenenManager : MonoBehaviour
     /// </summary>
     protected int _activeLevelSceneIndex = -1;
 
-    /// <summary>
-    /// Holds all build indices of scenens
-    /// </summary>
-    protected List<int> _levelScenes = new List<int>();
-
-
-
     #endregion Properties and Fields
 
 
     #region Methods
 
     /// <summary>
-    /// Add a level to the level scene list
+    /// Loads the start menu scene and unloads all others
     /// </summary>
-    /// <param name="buildIndex">Build index of the scene</param>
-    public void AddLevelScene(int buildIndex)
+    public void LoadStartMenuScene()
     {
-        _levelScenes.Add(buildIndex);
+        LoadScene(startMenuScene, false, _activeLevelSceneIndex, baseScene);
     }
 
 
     /// <summary>
-    /// Add multiple levels to the scene list
+    /// Loads first level from the <c>LevelScenes</c> list
     /// </summary>
-    /// <param name="buildIndices">Build indices (order is important)</param>
-    public void AddLevelScenes(int[] buildIndices)
+    public void LoadFirstLevel()
     {
-        foreach (int buildIindex in buildIndices) 
-            _levelScenes.Add(buildIindex);
+        LoadLevel(0);
     }
 
 
+    /// <summary>
+    /// Loads the n-th level from the <c>LevelScenes</c> list
+    /// </summary>
+    /// <param name="listIndex">Index of the <c>LevelScenes</c> list</param>
+    public void LoadLevel(int listIndex)
+    {
+        LoadScene(levelScenes[listIndex], true, _activeLevelSceneIndex);
+    }
 
-    public void LoadNextLevelScene()
+
+    /// <summary>
+    /// Loads the next level from the <c>LevélScenes</c> list
+    /// </summary>
+    public void LoadNextLevel()
     {
         // Check if there isn't a next level scene
-        if (_activeLevelSceneIndex >= _levelScenes.Count - 1)
+        if (_activeLevelSceneIndex >= levelScenes.Count - 1)
         {
             throw new Exception("No next level scene. Index out of range.");
         }
 
-        // Unload the current active level
-        var asyncOp =  SceneManager.UnloadSceneAsync(_levelScenes[_activeLevelSceneIndex]);
-        // Load the next level
-        asyncOp.completed += (AsyncOperation aO) => SceneManager.LoadScene(_levelScenes[++_activeLevelSceneIndex], LoadSceneMode.Additive);
+        LoadLevel(++_activeLevelSceneIndex);
+    }
+
+
+    private void LoadScene(int newScene, bool loadWithBase, int oldScene = -1, int oldScene2 = -1)
+    {
+        if (loadWithBase)
+            StartCoroutine(LevelTransition(newScene, oldScene, oldScene2));
+        else
+            StartCoroutine(LoadSceneTransition(newScene, oldScene, oldScene2));
+    }
+
+
+    private IEnumerator LevelTransition(int newScene, int oldScene = -1, int oldScene2 = -1)
+    {
+        //Level End Fade to Black
+        AsyncOperation unload;
+        AsyncOperation load;
+
+        if (oldScene != -1)
+        {
+            unload = SceneManager.UnloadSceneAsync(oldScene);
+            yield return unload;
+        }
+
+        if (oldScene2 != -1)
+        {
+            unload = SceneManager.UnloadSceneAsync(oldScene2);
+            yield return unload;
+        }
+
+        load = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+
+        yield return load;
+        yield return new WaitForEndOfFrame();
+        if (!SceneManager.GetSceneByName("Base").isLoaded)
+        {
+            load = SceneManager.LoadSceneAsync("Base", LoadSceneMode.Additive);
+            yield return load;
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+
+
+    private IEnumerator LoadSceneTransition(int newScene, int oldScene = -1, int oldScene2 = -1)
+    {
+        AsyncOperation unload;
+        AsyncOperation load;
+
+        if (oldScene != -1)
+        {
+            unload = SceneManager.UnloadSceneAsync(oldScene);
+            yield return unload;
+        }
+
+        if (oldScene2 != -1)
+        {
+            unload = SceneManager.UnloadSceneAsync(oldScene2);
+            yield return unload;
+        }
+
+        load = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+
+        yield return load;
+        yield return new WaitForEndOfFrame();
     }
 
 
@@ -67,6 +160,22 @@ public class ScenenManager : MonoBehaviour
 
 
     #region MonoBehaviour
+
+    private void Start()
+    {
+        switch (startScene)
+        {
+            case SceneType.StartMenu:
+                LoadStartMenuScene();
+                break;
+            case SceneType.Level:
+                LoadFirstLevel();
+                break;
+            default:
+                LoadStartMenuScene();
+                break;
+        }
+    }
 
 
     #endregion MonoBehaviour
